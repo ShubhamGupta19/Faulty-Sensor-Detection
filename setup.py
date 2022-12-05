939cf087726d7639dc090283a0b65ec67ba571af
ShubhamGupta19 <shubhamgupta2048@gmail.com>
2022-12-05 00:38:44 -0500

    Project Structure setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Shubham",
+    author_email="shubhamgupta2048@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
