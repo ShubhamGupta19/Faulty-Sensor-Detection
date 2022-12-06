78fbaf6f642c4f4b4051f0d592c399ba6b0613d0
ShubhamGupta19 <shubhamgupta2048@gmail.com>
2022-12-05 20:50:33 -0500

    MongoDB tested

--- sensor/configuration/mongodb_connection.py
+++ sensor/configuration/mongodb_connection.py
@@ -7,10 +7,11 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME)->None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url="mongodb+srv://user1:user1@sensortopic.fulcw0i.mongodb.net/?retryWrites=true&w=majority"
+                mongo_db_url="mongodb+srv://user1:mypassword@sensortopic.fulcw0i.mongodb.net/?retryWrites=true&w=majority"
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client=MongoDBClient.client
             self.database=self.client[database_name]
+            #print(self.database)
             self.database_name=database_name
         except Exception as e:
             raise e
