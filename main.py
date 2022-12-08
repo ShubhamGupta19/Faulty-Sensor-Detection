c187ec006b0134585152c5b30255d96fd79c6058
ShubhamGupta19 <shubhamgupta2048@gmail.com>
2022-12-08 17:22:13 -0500

    updated

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from sensor.configuration.mongodb_connection import MongoDBClient
+from sensor.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
 
 if __name__ == '__main__':
-    mongo_client = MongoDBClient()
-    print(mongo_client.database.list_collection_names())
+    training_pipeline_config=TrainingPipelineConfig()
+    data_ingestion_config=DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    print(data_ingestion_config.__dict__)
+    
+
